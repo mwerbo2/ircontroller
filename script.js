9797f95e6cae96441dc3851152577547c3861218
Michael Werbowsky <mwerbo2@gmail.com>
2019-12-20 23:55:37 -0600

    Added event listeners and changing of elements

--- script.js
+++ script.js
@@ -18,8 +18,51 @@ function ready(callbackFunc) {
 
 
 ready(function () {
+  const time = new Date;
 
-  document.getElementById('connect-hdmi').addEventListener('click', async () => {
+  let state = {
+    inputSelected: "",
+    volume: 50,
+    error: "",
+    currentTime: time.toLocaleTimeString()
+  }
+
+  setInterval(() => {
+    document.getElementById('header_time').innerHTML = state.currentTime;  
+  }, 1000);
+
+// Connect via HDMI
+  document.getElementById('hdmi').addEventListener('click', () => {
+    state.inputSelected = "hdmi"
+
+    document.getElementById('hdmi').addEventListener('click', async () => {
+      const projectorOn = await axios.post('api/v1/irports/1/senddi', { "frequency": 35955, "irCode": "323,161,20,20,20,60,20,20,20,20,20,60,20,60,20,20,20,20,20,60,20,20,20,60,20,60,20,20,20,20,20,60,20,60,20,20,20,60,20,20,20,20,20,20,20,20,20,20,20,20,20,60,20,20,20,60,20,60,20,60,20,60,20,60,20,60,20,1436,322,80,20,3500", "preamble": "", "repeat": 1 })
+      console.log("Projector status ", projectorOn.status)
+      if (projectorOn.status !== 200) {
+        alert("Error turning on projector")
+      } 
+         const receiverOn = await axios.post('api/v1/irports/2/senddir', {"frequency":38186,"irCode":"342,170,22,21,22,63,22,21,22,63,22,63,22,63,22,63,22,21,22,63,22,21,22,63,22,21,22,21,22,21,22,21,22,63,22,21,22,21,22,21,22,21,22,21,22,21,22,21,22,21,22,63,22,63,22,63,22,63,22,63,22,63,22,63,22,21,22,1556,342,85,22,3647,342,85,22,3800","preamble":"","repeat":1})
+        receiverOn.status !== 200
+        console.log("Receiver on", receiverOn)
+      })
+    
+  })
+  
+  document.getElementById('chromecast').addEventListener('click', () => {
+    state.inputSelected = "chromecast"
+    document.getElementById('home').style.display = 'none';  
+  })
+
+  document.getElementById('home_button').addEventListener('click', () => {
+    state.inputSelected = "home"
+  })
+
+  document.getElementById('power_button').addEventListener('click', () => {
+    
+  })
+
+  
+  document.getElementById('hdmi').addEventListener('click', async () => {
   const projectorOn = await axios.post('api/v1/irports/1/senddi', { "frequency": 35955, "irCode": "323,161,20,20,20,60,20,20,20,20,20,60,20,60,20,20,20,20,20,60,20,20,20,60,20,60,20,20,20,20,20,60,20,60,20,20,20,60,20,20,20,20,20,20,20,20,20,20,20,20,20,60,20,20,20,60,20,60,20,60,20,60,20,60,20,60,20,1436,322,80,20,3500", "preamble": "", "repeat": 1 })
   console.log("Projector status ", projectorOn.status)
   if (projectorOn.status !== 200) {
